ba638d2f83d28720c547f2a7274609d216664d45
andrewjensen <andrew.jensen-battaglia@jorsek.com>
2017-03-20 09:10:11 -0400

    hooked up type-ahead start and destination search suggestions

--- js/routeCast.js
+++ js/routeCast.js
@@ -1,4 +1,6 @@
-//----------------GLOBAL VARS--------------------------------------
+
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const cities = [];
 
 var directionsDisplay, map, origin1, destinationA,
     routeArray, routeLength, startLat, startLng, endLat, endLng,
@@ -197,6 +199,50 @@ function weatherReads(routeDist) {
 
   getWeather();
 }
+
+//---------------------- SEARCH TYPE-AHEAD ------------------------
+
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => cities.push(...data));
+  
+function findMatches(wordToMatch, cities) {
+  return cities.filter(place => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex);
+  }); 
+}
+
+function numberWithCommas(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function displayMatches(e) {
+  const matchArray = findMatches(e.target.value, cities);
+  const html = matchArray.map( place => {
+    const regex = new RegExp(e.target.value, 'gi'),
+          cityName = place.city.replace(regex, `<span class="hl">${e.target.value}</span>`),
+          stateName = place.state.replace(regex, `<span class="hl">${e.target.value}</span>`),
+          pop = numberWithCommas(place.population);
+    
+    return `
+      <li>
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">${pop}</span>
+      </li>
+    `;
+  }).join('');
+  
+  e.target.nextElementSibling.innerHTML = html;
+}
+
+const searchInput = document.querySelectorAll('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.forEach(input => input.addEventListener('change', displayMatches));
+searchInput.forEach(input => input.addEventListener('keyup', displayMatches));
+
+
 
 //-------------------------------------------------------------------------------------------
 
